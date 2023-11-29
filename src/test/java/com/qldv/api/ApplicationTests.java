343e6da9ec23f8966be52a40a3b8d29a131eeb39
anhthu1402 <20520792@gm.uit.edu,vn>
2023-11-29 08:07:52 +0700

    add files

---
+++ src/test/java/com/qldv/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qldv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
