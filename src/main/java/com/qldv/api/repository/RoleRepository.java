2b7ef214120b35c035f672840ff55e7b6c368460
Trinh Truong <20520825@gm.uit.edu.vn>
2023-12-05 22:29:37 +0700

    Role

--- src/main/java/com/qldv/api/repository/RoleRepository.java
+++ src/main/java/com/qldv/api/repository/RoleRepository.java
@@ -1,5 +1,9 @@
 package com.qldv.api.repository;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qldv.api.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
 
 }
--- src/main/java/com/qldv/api/service/RoleService.java
+++ src/main/java/com/qldv/api/service/RoleService.java
@@ -1,5 +1,62 @@
 package com.qldv.api.service;
 
-public class RoleService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qldv.api.model.Role;
+import com.qldv.api.model.User;
+import com.qldv.api.repository.RoleRepository;
+
+@Service
+public class RoleService {
+	@Autowired
+	RoleRepository roleRepository;
+	
+	//create role
+	public Role createRole(Role role) {
+		return roleRepository.save(role);
+	}
+	
+	//get all role
+	public List<Role> getAllRoles(){
+		return roleRepository.findAll();
+	}
+	
+	//get role by id
+	public Role getRoleById(Integer id) {
+		Optional<Role> role = roleRepository.findById(id);
+		if(role.isPresent()) {
+			return role.get();
+		}
+		return new Role();
+	}
+	
+	//update role
+	public Role updateRole(Integer id, Role roleDetail) {
+		Optional<Role> role = roleRepository.findById(id);
+		if(role.isPresent()) {
+			Role r = role.get();
+			r.setType(roleDetail.getType());
+			return roleRepository.save(r);
+		}
+		return roleRepository.save(roleDetail);
+	}
+	
+	//delete role
+	public Boolean deleteRole(Integer id) {
+		Optional<Role> role = roleRepository.findById(id);
+		if(role.isPresent()) {
+			Role r = role.get();
+			List<User> users = r.getListUsers();
+			for (User user : users) {
+				user.setRole(null);
+			}
+			roleRepository.delete(r);
+			return true;
+		}
+		return false;
+	}
 }
